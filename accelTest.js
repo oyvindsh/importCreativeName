40bb443dc920ba316ba33aac1e3d0b38e2cc1663
Øyvind Haarberg <oyvind.haarberg@gmail.com>
2016-10-10 20:32:09 +0200

    naa gjoer vi alt

--- accelTest.js
+++ accelTest.js
@@ -1,4 +1,5 @@
 var SensorTag = require('sensortag');
+var fs = require('fs');
 
 var log = function(text) {
   if(text) {
@@ -8,6 +9,8 @@ var log = function(text) {
 
 var xTresh = -0.7;
 var zTresh = 0.5;
+var timeout = false;
+var kaffeLevel = 1000;
 
 // (replace the one below).
 var ADDRESS = "b0:b4:48:bd:ce:04";
@@ -36,10 +39,10 @@ sensor.then(function(tag) {
   });
 });
 
-var lastHelling = 0;
-var timeout = false;
+var kaffeSkruddPaa = false;
 
 var handleKaffe = function () {
+  kaffeSkruddPaa = false;
   if (timeout) {
     return;
   }
@@ -52,11 +55,31 @@ var handleKaffe = function () {
     log("tok bilde");
   });
   setTimeout(function () {
-    run_cmd ('python', ["tweeter.py", "Test 42", "image.jpg"], function () {
-      log("oida");
+    kaffeSkruddPaa();
+  }, 450000);
+  setTimeout(function () {
+      if (!kaffeSkruddPaa) {
+        run_cmd ('python', ["tweeter.py", "#kaffesynder tom for kaffe og lagde ikke ny #fuii #importCreativeName", "image.jpg"], function () {log("oida"); });
+      } else {
+        run_cmd ('python', ["tweeter.py", "#kaffepride tom for kaffe og lagde ny! #fuii #importCreativeName", "image.jpg"], function () {log("oida");});
+          setTimeout(function () {
+            run_cmd ('python', ["tweeter.py", "Nå er kaffen klar! #fuii #importCreativeName", "kaffe.jpg"], function () {log("oida");});
+          }, 500000);
     });
-  }, 5000);
+  }, 300000);
 };
+
+function kaffeSkruddPaa() {
+  run_cmd('wallplug_power.sh',[], function () {
+    //
+  });
+  setTimeout(function () {
+    var obj = JSON.parse(fs.readFileSync('wallplug_power.json','utf8'));
+    if (obj.data.metrics.level > kaffeLevel) {
+      kaffeSkruddPaa = true;
+    }
+  }, 500);
+}
 
 function run_cmd(cmd, args, callBack ) {
     var spawn = require('child_process').spawn;
